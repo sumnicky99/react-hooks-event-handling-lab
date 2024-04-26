f4b5e73f5b720e7cd7d6c4897d490f3d2f66985d
Sumnicky99 <sumnicky99@gmail.com>
2024-04-26 16:40:34 +0300

    done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+
+export default function EyesOnMe() {
+    return (
+        <div>
+            <button onFocus={getFocus} onBlur={getBlur}>Eyes on me</button>
+        </div>
+    )
+}
+
+function getFocus(){
+    return console.log("Good!")
+}
+
+function getBlur() {
+    return console.log("Hey! Eyes on me!")
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,20 @@
 // Code Keypad Component Here
 
+
+import React from 'react';
+
 function Keypad (){
+    function handleChange() {
+        console.log('Entering password...')
+    }
     return (
-        <div></div>
+        <div>
+            <input 
+                type="password"
+                onChange={handleChange}
+            />
+        </div>
     )
 }
 
-export default Keypad;
+export default Keypad; 
